c257e064b338c2c78506a5338c34d07ba9998394
OfficePigeon <ava@wich.fun>
2026-01-04 21:19:59 -0500

    fixed versions and package script

--- package.py
+++ package.py
@@ -1,5 +1,8 @@
 import os
 import shutil
 
+for archive in os.listdir('builds'):
+	if archive.endswith('.zip'):
+		os.remove('builds/' + str(archive))
 for version in os.listdir('versions'):
 	shutil.make_archive('builds/SaplingsToDeadBushes' + str(version), 'zip', 'versions/' + str(version))
